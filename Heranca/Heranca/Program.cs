d4e6511e12940ee9b91ed7141a1f8e0f6f7ffba5
Karolina Gomes <karolina_bp@hotmail.com>
2020-04-24 15:20:12 -0300

    Atualizado heranca com upcasting e downcasting

--- Heranca/Heranca/Program.cs
+++ Heranca/Heranca/Program.cs
@@ -7,11 +7,45 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brow", 100.0, 500.0);
+            //BusinessAccount account = new BusinessAccount(8010, "Bob Brow", 100.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            //Console.WriteLine(account.Balance);
 
             //account.Balance = 200.00;
+
+            Account acc = new Account(1001, "Alex", 0.0);
+
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING = conversão da sub-classe para super-classe:
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING = conversão da super-classe para sub-classe:
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                //Outra forma de converter:
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
