c81f6028f1d2d2ccdd94270c8681d7c0fd18327a
Karolina Gomes <karolina_bp@hotmail.com>
2020-04-30 16:02:06 -0300

    Adionado exercicios polimorfismo, heranca e projeto IncomeTax

---
+++ ExClassesMetodosAbstratos/ExClassesMetodosAbstratos/Entities/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using ExClassesMetodosAbstratos.Entities.Enums;
+
+namespace ExClassesMetodosAbstratos.Entities
+{
+    //Classe herdando de uma abstract obrigatoriamente tem que implementar o método que é abstract na outra:
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
---
+++ ExClassesMetodosAbstratos/ExClassesMetodosAbstratos/Entities/Shape.cs
@@ -0,0 +1,18 @@
+﻿using ExClassesMetodosAbstratos.Entities.Enums;
+
+namespace ExClassesMetodosAbstratos.Entities
+{
+    abstract class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        //Método abstract já vem com o override, por isso nao boto o virtual aqui:
+        //método abstrato não tem corpo:
+        public abstract double Area();
+    }
+}
