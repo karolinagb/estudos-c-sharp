123af82dbcc6c4304dac5a3253dcca75a9248fa6
Karolina Gomes <karolina_bp@hotmail.com>
2020-05-07 18:24:55 -0300

    Adicionado projeto exceções

---
+++ ExcecoesPersonalizadas/ExcecoesPersonalizadas/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExcecoesPersonalizadas.Entities.Exceptions
+{
+    //ApplicationException permite a criação de exceções personalizadas
+    //A palavra domain é para identificar que a exceção faz parte do domínio do negócio
+    class DomainException : ApplicationException
+    {
+        public DomainException (string message) : base (message)
+        {
+
+        }
+    }
+}
---
+++ ExcecoesPersonalizadas/ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Exceptions;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try para tentar executar o bloco de comandos
+            try { 
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Enter data to update the reservation:");
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            checkOut = DateTime.Parse(Console.ReadLine());
+
+            reservation.UpdateDates(checkIn, checkOut);
+            Console.WriteLine("Reservation: " + reservation);
+
+            }
+            //Se der algum problema na tentativa, capturar exceção usando a cláusula catch
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+        }
+    }
+}
+
