46f0ae4d6dce8fcc3332ef7f9b3197b0162d085b
Karolina Gomes <karolina_bp@hotmail.com>
2020-05-18 18:01:06 -0300

    Adicionado projeto arquivos parte IV

---
+++ ArquivosParteIV/ArquivosParteIV/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ArquivosParteIV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\projetos\file1.txt";
+            string targetPath = @"c:\projetos\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                //O AppendText abre o arquivo para escrita no final do mesmo
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        /*Cada linha do vetor lines (que recebeu o sourcePath vai ser escrita dentro
+                         do targetPath em maiusculo (ToUpper). Depois o arquivo será aberto para eu escrever algo no final*/
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
