2caf9ca004b1bb3887daac3980c82a02603c581c
Karolina Gomes <karolina_bp@hotmail.com>
2020-05-29 16:18:34 -0300

    Atualizado projeto Heranca Multipla

---
+++ HerancaMultipla/HerancaMultipla/Devices/Printer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaMultipla.Devices
+{
+    //Posso herdar de uma classe e também implementar uma interface
+    class Printer : Device, IPrinter
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Printer processing: " + document);
+        }
+        public void Print(string document)
+        {
+            Console.WriteLine("Printer print: " + document);
+        }
+    }
+}
