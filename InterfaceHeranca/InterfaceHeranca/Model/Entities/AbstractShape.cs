d0f7f8853ad4d5dc79a79faffabd2710d95de56d
Karolina Gomes <karolina_bp@hotmail.com>
2020-05-29 14:39:32 -0300

    Adicionado projeto InterfaceHeranca

---
+++ InterfaceHeranca/InterfaceHeranca/Model/Entities/AbstractShape.cs
@@ -0,0 +1,13 @@
+﻿using InterfaceHeranca.Model.Enums;
+
+namespace InterfaceHeranca.Model.Entities
+{
+    abstract class AbstractShape : IShape
+    {
+        public Color Color { get; set; }
+
+        /*Mesmo sendo uma classe abstrata, o C# exige que a mesma implemente a interface. 
+         * Só que ela pode ser implementada de forma abstrata também:*/
+        public abstract double Area();
+    }
+}
---
+++ InterfaceHeranca/InterfaceHeranca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InterfaceHeranca.Model.Entities;
+using InterfaceHeranca.Model.Enums;
+
+namespace InterfaceHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Como não fiz construtor na classe Circle, posso instanciar da forma que segue: */
+            //OBS: Color = Color.White = A propriedade Color recebe o enum Color.White
+            IShape s1 = new Circle() { Color = Color.White, Radius = 2.0 };
+            IShape s2 = new Rectangle() { Width = 3.5, Height = 4.2, Color = Color.Black};
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
