38f671d338010dcdcb3d04e3bc5ce65b4d410dae
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-01 14:04:29 -0300

    Projeto IComparable

---
+++ ComparacaoObjetos/ComparacaoObjetos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ComparacaoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    //list.Sort = para ordenar uma lista
+                    list.Sort();
+                    foreach(string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }   
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
