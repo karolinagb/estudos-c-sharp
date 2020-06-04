989bc78b5bfa627b7f8f5d0072ef98fe2dfbd64d
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-04 17:44:32 -0300

    Adicionado projeto RestricoesGenerics

---
+++ RestricoesGenerics/RestricoesGenerics/Entities/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace RestricoesGenerics.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            //Testando se o objeto que chegou como parâmetro é um Product
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Comparing erro: Argument is not a Product");
+            }
+
+            //Se for produto, então receber
+            Product other = (Product) obj;
+
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ RestricoesGenerics/RestricoesGenerics/Services/CalculationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestricoesGenerics.Services
+{
+    class CalculationService
+    {
+        //Posso colocar o método como generico tambem assim: public T Max<T>
+        //Where T: IComparable = permite tipo genericos que implementem a interface IComparable, ou seja, 
+        //que tenham o método CompareTo (possam ser comparados)
+        public T Max<T> (List<T> list) where T : IComparable
+        {
+            //Verificando se a lista está vazia:
+            if(list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty!");
+            }
+
+            //Guardando o primeiro elemento da lista a fim de fazer as comparações
+            T max = list[0];
+
+            //Percorrendo o restante dos itens para ver qual é maior
+            //list.Count = todos os elementos da lista até o final
+            for (int i = 1; i < list.Count; i++)
+            {
+                //Comparando max com list e vendo se é maior que zero = se o elemento no parametro for maior que
+                //o elemento atual, então o elemento do parâmetro do CompareTo é maior que o elemento atual
+                if(list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
