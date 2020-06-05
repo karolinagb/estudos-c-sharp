a81d0d4627ae8ff57603c33277399337716ece1e
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-05 15:54:11 -0300

    Adicionado projeto ConjuntosParteII - HashSet

---
+++ ConjuntosParteII/ConjuntosParteII/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConjuntosParteII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando um conjunto do tipo HashSet
+            HashSet<string> set = new HashSet<string>();
+
+            //Adicionando itens ao conjunto
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            //Verificando se há um elemento no conjunto
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
