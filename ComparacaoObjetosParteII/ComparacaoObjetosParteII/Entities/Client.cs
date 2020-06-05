133fba63199cd1da7667242844fe1d1237fcdf58
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-05 12:18:33 -0300

    Adicionado projeto ComparacaoObjetosParteII

---
+++ ComparacaoObjetosParteII/ComparacaoObjetosParteII/Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComparacaoObjetosParteII.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //Para customizar o método Equals, temos que sobrescrevê-lo
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+
+            Client other = obj as Client;
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ ComparacaoObjetosParteII/ComparacaoObjetosParteII/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ComparacaoObjetosParteII.Entities;
+
+namespace ComparacaoObjetosParteII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+
+            //O == compara a referência de memória e não o conteúdo e portanto ele continua dando sempre diferente os objetos
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
