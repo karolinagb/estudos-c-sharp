80a6feea03c2531773c400da8fa4f58bd1bb133a
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-08 17:06:25 -0300

    Adicionado projeto exercicios sobre conjuntos parte II

---
+++ ExercicioFixacaoConjuntosParteII/ExercicioFixacaoConjuntosParteII/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExercicioFixacaoConjuntosParteII.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExercicioFixacaoConjuntosParteII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Vou usar o mais rápido nesse caso que é o HashSet, já que a ordem não importa*/
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord(name, instant));
+                    }
+                    //set.Count traz a quantidade de elementos do conjunto:
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
