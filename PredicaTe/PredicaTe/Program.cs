f0dc4f793bfb7f1975788f4b1b76737c4a40b888
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-11 17:12:31 -0300

    Aula 223 - Predicate Parte I e II

---
+++ PredicaTe/PredicaTe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PredicaTe.Entities;
+using System.Collections.Generic;
+
+namespace PredicaTe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            /*O RemoveAll possui um argumento que é o Predicate. 
+             O Predicate é uma função que recebe um objeto e devolve um booleano.*/
+            /*>= tal que
+             p => p.Price >= 100.0  -- é uma função lambda (função anônima) que recebe um produto p
+            e devolve o resultado da condição.*/
+            list.RemoveAll(p => p.Price >= 100.0);
+
+            /*VAMOS verificar a exclusão:*/
+            foreach (Product pr in list)
+            {
+                Console.WriteLine(pr);
+            }
+        }
+    }
+}
---
+++ PredicaTeParteII/PredicaTeParteII/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PredicaTeParteII.Entities;
+using System.Collections.Generic;
+
+namespace PredicaTeParteII
+{
+    class Program
+    {
+        //Versão sem a função lambda como argumento do RemoveAll
+
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+
+            /*VAMOS verificar a exclusão:*/
+            foreach (Product pr in list)
+            {
+                Console.WriteLine(pr);
+            }
+        }
+
+        /*Depois do método Main eu vou criar uma função que recebe um produto e devolve um booleano*/
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
