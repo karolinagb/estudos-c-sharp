5875cc0dd9accbf1acc4736fb3798a9c5d9b301b
Karolina Gomes <karolina_bp@hotmail.com>
2020-06-11 16:40:51 -0300

    Aula 222 - Multicast Delegates

---
+++ MultiCastDelegates/MultiCastDelegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MultiCastDelegates.Services;
+namespace MultiCastDelegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //Referenciando dois métodos
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            //Como ele não tem retorno, então não posso recebê-lo na variável
+            op.Invoke(a, b);
+            //Pode ser também op(a, b)
+        }
+    }
+}
